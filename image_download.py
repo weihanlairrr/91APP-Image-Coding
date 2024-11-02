593699ff8d348a668349a9d5b1d2375d5cc7d51a
weihanlairrr <141799681+weihanlairrr@users.noreply.github.com>
2024-11-02 14:23:54 +0800

    Update image_download.py

--- image_download.py
+++ image_download.py
@@ -6,7 +6,7 @@ import json
 from pathlib import Path
 
 # 輸入的 JSON 資料
-json_input = '["JF1367"]'
+json_input = '["JF1367",]'
 
 # 根據貨號產生 10 張圖片的網址
 def generate_urls(catalog):
@@ -29,7 +29,7 @@ def download_images(urls, folder_name):
             with open(os.path.join(folder_name, f'image_{i+1}.jpg'), 'wb') as handler:
                 handler.write(response.content)
         else:
-            pass
+            print(f"圖片 {url} 不存在，跳過下載。")
 
 # 獲取使用者的 Downloads 資料夾路徑
 def get_downloads_folder():
