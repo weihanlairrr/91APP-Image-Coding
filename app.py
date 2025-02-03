bbd9e4a03ece2b4e49cfc606df7a25b3ec220aa3
weihanlairrr <141799681+weihanlairrr@users.noreply.github.com>
2025-02-03 16:22:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@
 import streamlit as st
 import os
 import faiss  
-import 自動編圖
-import 編圖複檢
-from style import custom_css
+import dependencies.自動編圖
+import dependencies.編圖複檢
+from dependencies.style import custom_css
 
 st.set_page_config(page_title='TP自動編圖工具', page_icon='👕', layout="wide")
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
@@ -14,8 +14,8 @@ st.markdown(custom_css, unsafe_allow_html=True)
 tab1, tab2 = st.tabs(["自動編圖", "編圖複檢"])
 
 with tab1:
-    自動編圖.tab1()
+    dependencies.自動編圖.tab1()
 
 with tab2:
-    編圖複檢.tab2()
+    dependencies.編圖複檢.tab2()
  
