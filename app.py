226bbe492096c21817c620a83d122406f7efe633
weihanlairrr <141799681+weihanlairrr@users.noreply.github.com>
2025-04-21 18:06:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ import logging
 import faiss  
 import 自動編圖
 import 編圖複檢
-
+import 圖片訓練
+import 批次換圖
 from style import custom_css
 
 st.set_page_config(page_title='TP自動編圖工具', page_icon='👕', layout="wide")
@@ -22,11 +23,16 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 faiss.omp_set_num_threads(1)
 st.markdown(custom_css, unsafe_allow_html=True)
 
-tab1, tab2 = st.tabs(["自動編圖", "編圖複檢"])
+tab1, tab2, tab3, tab4 = st.tabs(["自動編圖", "編圖複檢", "圖片訓練","ADS批次換圖"])
 
 with tab1:
     自動編圖.tab1()
 
 with tab2:
     編圖複檢.tab2()
+
+with tab3:
+    圖片訓練.tab3()
  
+with tab4:
+    批次換圖.tab4()
